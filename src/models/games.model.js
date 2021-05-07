9cb6196f29ce2787534398e45422d2dfd296a17a
iago <iagolol@outlook.com>
2021-05-07 16:07:30 -0300

    feat: tags on games

--- src/models/games.model.js
+++ src/models/games.model.js
@@ -7,14 +7,14 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
 
-  const Tags = new Schema({
-    name: { type: String, required: true },
-    value: { type: Number, required: true }
+  const tagSchema = new Schema({
+    tagId: { type: Schema.Types.ObjectId, ref: 'tags'}, 
+    name:{ type: String, required: true },
   });
 
   const schema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'users'},//required
-    tags: Tags,
+    tags: [{type: tagSchema}],
     title: { type: String, required: true },
     description: {type: String},
     members: { type: String},
